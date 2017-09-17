e3c55822777f1fe43a3195c4f51de26390855eeb
Michael Hladky <michael@hladky.at>
2017-09-17 15:18:06 +0200

    feat(Manage state with @ngrx/store): BONUS Implement findPending state to store - dispatch actions and subscribe to state
    Related to #1

--- src/app/pages/flight/services/flight.service.ts
+++ src/app/pages/flight/services/flight.service.ts
@@ -13,28 +13,25 @@ export class FlightService {
 
   readonly flights$: Observable<Flight[]>
 
-  private _isFindPending$: BehaviorSubject<boolean> = new BehaviorSubject(false)
   readonly isFindPending$: Observable<boolean>
 
   constructor(
     private fr: FlightResource,
     private store: Store<IDB>
   ) {
-    this.flights$ = this.store.select<fromFlight.IFlightState>(fromFlight.getFlights)
-
-    this.isFindPending$ = this._isFindPending$.asObservable()
+    this.flights$ = this.store.select(fromFlight.getFlights)
+    this.isFindPending$ = this.store.select(fromFlight.getFindPending)
   }
 
   find(from?: string, to?: string) {
-    this._isFindPending$.next(true)
+    this.store.dispatch(new flight.FindAction())
     this.fr.find(from, to)
       .subscribe(
         n => {
           this.store.dispatch(new flight.FindSuccessAction(n))
-          this._isFindPending$.next(false)
         },
         e => {
-          this._isFindPending$.next(false)
+          this.store.dispatch(new flight.FindFailAction())
         })
   }
 
