adc361ef2ff4a534e422071402e164ddce21827b
Michael Hladky <michael@hladky.at>
2017-10-16 01:10:12 +0200

    remove console.log

--- src/app/state-management/router-state.serializer.ts
+++ src/app/state-management/router-state.serializer.ts
@@ -1,6 +1,5 @@
-
+import {RouterStateSnapshot} from '@angular/router';
 import {RouterStateSerializer} from '@ngrx/router-store';
-import {Params, RouterStateSnapshot} from '@angular/router';
 
 export interface IRouterStateUrl {
   url: string;
@@ -10,13 +9,11 @@ export interface IRouterStateUrl {
 export class CustomSerializer implements RouterStateSerializer<IRouterStateUrl> {
 
   serialize(routerState: RouterStateSnapshot): IRouterStateUrl {
-    const { url } = routerState;
+    const {url} = routerState;
     const params = routerState.root.firstChild.params;
-
-    console.log('routerState', routerState)
 
     // Only return an object including the URL and query params
     // instead of the entire snapshot
-    return { url, params };
+    return {url, params};
   }
 }
