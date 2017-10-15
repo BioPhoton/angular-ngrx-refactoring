7ed12a746d480f0c500176484eb07d55320a54ab
Michael Hladky <michael@hladky.at>
2017-10-16 01:04:42 +0200

    feat(Using ngrx in feature modules): Implement `Store`, `Effects`, `DevTools` and `RooterStore` in the newly created `StateManagementModule`.
    Related to #6

--- src/app/state-management/state-management.module.ts
+++ src/app/state-management/state-management.module.ts
@@ -1,11 +1,33 @@
 import {NgModule, Optional, SkipSelf} from '@angular/core';
+import {EffectsModule} from '@ngrx/effects';
+import * as fromRouter from '@ngrx/router-store';
+import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+
+import {CustomSerializer} from './router-state.serializer';
+
+const reducer = {
+  routerBranch: fromRouter.routerReducer
+}
+
+const effects = []
 
 @NgModule({
   imports: [
+    StoreModule.forRoot(reducer),
+    StoreDevtoolsModule.instrument({
+      maxAge: 10 //  Buffers the last 10 states
+    }),
+    EffectsModule.forRoot(effects),
+    fromRouter.StoreRouterConnectingModule
   ],
   exports: [
+    StoreModule,
+    StoreDevtoolsModule,
+    EffectsModule
   ],
   providers: [
+    {provide: fromRouter.RouterStateSerializer, useClass: CustomSerializer}
   ],
 })
 export class StateManagementModule {
